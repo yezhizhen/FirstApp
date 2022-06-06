b832d3b4994245beac45ef11c574396ffb30a52b
Euclid <yezhizhenjiakang@gmail.com>
2022-06-06 19:37:53 +0800

    Initial commit
    First Commit. Create a view. View Binding

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.firstTv1.setOnClickListener {
+            binding.firstTv1.text = "Bazinga!"
+        }
+        //
+
+    }
+}
